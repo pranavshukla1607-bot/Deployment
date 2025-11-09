84bad120aa066db6f51dd1e4324d2d2e51be3c59
Pranave8299 <76514258+Pranave8299@users.noreply.github.com>
2025-11-09 14:47:06 +0530

    Initial commit

---
+++ springDemo/src/main/java/com/example/springDemo/Controller/SpringContoller.java
@@ -0,0 +1,16 @@
+package com.example.springDemo.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class SpringContoller {
+
+    @GetMapping("/get")
+    public String getMap(){
+
+        return "ok";
+    }
+}
